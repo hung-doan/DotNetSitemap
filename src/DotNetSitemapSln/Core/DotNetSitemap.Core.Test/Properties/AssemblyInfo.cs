36abad6aa8d7cf6b562034fe07527f1115c228bd
Hung Doan <hung.doantan@gmail.com>
2017-08-01 01:18:18 +0700

    Init

---
+++ src/DotNetSitemapSln/Core/DotNetSitemap.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetSitemap.Core.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetSitemap.Core.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78b500de-c0f3-446e-8070-9cd8b2b7107e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/DotNetSitemapSln/Core/DotNetSitemap.Core.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace DotNetSitemap.Core.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            throw new Exception("TC have not been implemented");
+        }
+    }
+}
